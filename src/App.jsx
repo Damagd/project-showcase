917704ff98b2c6e683a24562d616cd8189aa09a6
Damagd <75856639+Damagd@users.noreply.github.com>
2025-11-19 11:24:26 -0600

    Cambio app
    Cambie app

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,31 @@
-import './App.css'
-import { Overlay } from './components/interface/Overlay.jsx'
-import { useContext } from 'react'
-import { DarkMode } from './components/interface/DarkMode.jsx'
-import { OptionsContext } from './context/optionsOverlay.jsx'
-import { ActivateOverlay } from './components/interface/ActivateOverlay.jsx'
-import { AppThreeDe } from './components/scene/AppThreeDe.jsx'
-import { OptionsSceneProvider } from './context/optionsScene.jsx'
+import "./App.css";
+import { Overlay } from "./components/interface/Overlay.jsx";
+import { useContext } from "react";
+import { DarkMode } from "./components/interface/DarkMode.jsx";
+import { OptionsContext } from "./context/optionsOverlay.jsx";
+import { ActivateOverlay } from "./components/interface/ActivateOverlay.jsx";
+import { AppThreeDe } from "./components/scene/AppThreeDe.jsx";
+import { OptionsSceneProvider } from "./context/optionsScene.jsx";
 
 function App() {
-  const {threeDe, overlay, setOverlay} = useContext(OptionsContext);
+  const { threeDe, overlay, setOverlay } = useContext(OptionsContext);
 
   const showOverlay = () => {
     setOverlay(true);
-  }
+  };
 
   return (
     <>
-      <DarkMode/>
-      { !overlay && <ActivateOverlay showOverlay={showOverlay} /> }
-      { 
-        threeDe && 
-        <OptionsSceneProvider> 
-          <AppThreeDe/> 
-        </OptionsSceneProvider> 
-      } 
-      { overlay && <Overlay /> }
+      <DarkMode />
+      {!overlay && <ActivateOverlay showOverlay={showOverlay} />}
+      {threeDe && (
+        <OptionsSceneProvider>
+          <AppThreeDe />
+        </OptionsSceneProvider>
+      )}
+      {overlay && <Overlay />}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
